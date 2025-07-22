a9ffab0d94c90074cccf5c80b7193d25172c4408
Jeshwanth Kumar A S <jeshwanthsaravanan@gmail.com>
2025-07-22 10:45:31 +0530

    feat: config for testing added

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,13 @@
-import { defineConfig } from 'vite';
+import { defineConfig } from 'vitest/config';
 import react from '@vitejs/plugin-react-swc';
 import svgr from 'vite-plugin-svgr';
 import path from 'path';
 
 // https://vite.dev/config/
 export default defineConfig({
+  test: {
+    globals: true,
+  },
   plugins: [
     react(),
     svgr({
